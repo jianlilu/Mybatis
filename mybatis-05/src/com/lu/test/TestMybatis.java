24b649e0ef41e0e8b2ee4abaa670501791fc3f8f
alilu1204 <1210195656@qq.com>
2017-05-23 11:57:08 +0800

    add file

---
+++ mybatis-05/src/com/lu/test/TestMybatis.java
@@ -0,0 +1,38 @@
+package com.lu.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.lu.pojo.Category;
+import com.lu.pojo.Product;
+
+
+public class TestMybatis {
+	  
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+  
+        List<Product> ps = session.selectList("listProduct");
+        for (Product p : ps) {
+            System.out.println(p+" 对应的分类是 \t "+ p.getCategory());
+        }
+ 
+        session.commit();
+        session.close();
+  
+    }
+}
+
+
+
